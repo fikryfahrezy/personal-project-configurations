6ac85dbf78a255d4a56ba2c021698324c78396c5
Fikry Fahrezy <ffahrezi69@gmail.com>
2021-08-13 10:05:19 +0700

    fix typo in scripts command

--- javascript-webpack/webpack.common.js
+++ javascript-webpack/webpack.common.js
@@ -33,7 +33,7 @@ module.exports = {
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: './',
     clean: true,
   },
 };
